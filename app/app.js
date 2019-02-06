6ff0afd33e6a758fc9f6db44e52f4f5fd35b7aad
Jurij Gasparovic <gasparovic@gmail.com>
2019-02-06 10:33:40 +0000

    another text added

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Cau svet, som tu!');
+  res.send('Cau svet, som tu a nebojim sa!');
 });
 
 app.listen(3000, function () {
